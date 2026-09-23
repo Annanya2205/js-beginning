cbf737c9a786ff10b7776cc28134f23922fb4778
Annanya2205 <145360580+Annanya2205@users.noreply.github.com>
2026-09-23 12:39:34 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 13445
+let accountEmail = "annanya@gmail.com"
+var accountPassword = "344"
+accountCity = "Gwalior"
+let accountState; //undefined
+// accountId=2
+
+accountEmail = "annanya1@gmail.com"
+accountPassword = "123456"
+accountCity = "delhi"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
