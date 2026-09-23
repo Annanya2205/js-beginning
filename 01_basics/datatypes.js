3d6fed381e344247bce9b06a60040b792d2f068a
Annanya2205 <145360580+Annanya2205@users.noreply.github.com>
2026-09-23 16:51:35 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+//alert( 3 + 3)
+
+let name = "annanya"
+let age = 21
+let isloggedIn = false
+
+//number => 2 to pwr 53
+// bigint 
+// string => ""
+//boolean => true/false
+//null = standalone value
+//undefined = value not assigned
+//symbol =  unique
+
+
+//object
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
