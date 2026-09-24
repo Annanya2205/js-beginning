6ecdbf9d09f0ad716b70a9e8b814f616602f199c
Annanya2205 <145360580+Annanya2205@users.noreply.github.com>
2026-09-24 16:39:33 +0000

    comparison

---
+++ 01_basics/comparisons.js
@@ -0,0 +1,11 @@
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === checks strictly(not only checks value but also checks dataType)
+
+console.log("2" === 2);
